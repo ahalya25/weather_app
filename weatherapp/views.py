b9deaf833f621edca37e574cece7cdc8becab17f
ahalya <ahalyamalu25@gmai.com>
2025-10-20 17:17:42 +0000

    showing correct weather for enterd city

--- weatherapp/views.py
+++ weatherapp/views.py
@@ -1,28 +1,25 @@
 from django.shortcuts import render
 import requests
-# Create your views here.
-def home(request):
 
+def home(request):
     weather_data = {}
     if request.method == 'POST':
-
         city = request.POST.get('city')
-        api_key = 'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}'
-        url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+        api_key = 'e194982fdd3da2b6368c9d45f50c02a7'
+        url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
         response = requests.get(url)
         data = response.json()
 
         if data.get('cod') == 200:
             weather_data = {
-
-                'city' : city ,
+                'city': city,
                 'temperature': data['main']['temp'],
                 'description': data['weather'][0]['description'],
                 'humidity': data['main']['humidity'],
                 'pressure': data['main']['pressure'],
-                'icon': data['weather'][0]['icon']
+                'icon': data['weather'][0]['icon'],
             }
         else:
-            weather_data['error'] = 'city not found'
+            weather_data['error'] = 'City not found'
 
-    return render(request,'weatherapp/home.html',{'weather_data':weather_data})        
+    return render(request, 'weatherapp/home.html', {'weather_data': weather_data})
